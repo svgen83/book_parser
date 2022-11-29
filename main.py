9b911464d4d28863b34b3dd57d8dd487729f43fe
svg83 <svgeneraloff@gmail.com>
2022-11-29 15:10:43 +0400

    добавлено скачивание нескольких книг

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
+import os
+import pathlib
+import random
 import requests
 
 
 
+def make_directory(directory_name):
+    pathlib.Path(directory_name).mkdir(parents=True, exist_ok=True)
 
-def download_file(file_name):
-    url = "https://tululu.org/txt.php?id=32168"
-    response = requests.get(url)
-    response.raise_for_status()
-    with open(file_name, "wb") as file:
-        file.write(response.content)
+def download_files(directory_name):
+    random.randint(10000, 99999)
+    id_books = [str(random.randint(10000, 99999)) for i in range(9)]
+    for id_book in id_books:
+        url = f"https://tululu.org/txt.php?id={id_book}"
+        response = requests.get(url)
+        response.raise_for_status()
+        book_name = f"{directory_name}/book_{id_book}.txt"
+        with open(book_name, "wb") as file:
+            file.write(response.content)
 
 
 if __name__ == "__main__":
 
-    download_file("book.txt")
+    directory_name = "./books"
+    make_directory(directory_name)
+    download_files(directory_name)
 
 
 
-
-
+###ссылка на книгу Пески Марса https://tululu.org/txt.php?id=32168
