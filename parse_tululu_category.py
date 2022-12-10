8af5d6252fce5d10252848aa17f39f0e2862d6f4
svgen83 <61458549+svgen83@users.noreply.github.com>
2022-12-10 19:59:46 +0400

    промежуточное сохранение

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -13,45 +13,78 @@ def check_for_redirect(response):
     if response.url == "https://tululu.org/":
         raise requests.exceptions.HTTPError
 
-def get_response(page_number):
-    responses = []
-    url = f"https://tululu.org/l55/{str(page_number)}"
+def get_response(url):
     response = requests.get(url)
     response.raise_for_status()
-    print(response.url)
     check_for_redirect(response)
     return response
 
 
-def parse_book_page(responses):
+def get_book_urls(responses):
     all_urls = []
     for response in responses:
         soup = BeautifulSoup(response.text,
                          "lxml")
         parsed_objs = soup.find("body").find("div", id = "content").find_all(
     "table")
-        id_urls = []
-        for parsed_obj in parsed_objs:
-            id_book = parsed_obj.find("a")["href"]
-            id_url = urljoin(response.url, id_book)
-            id_urls.append(id_url)
-        all_urls.append(id_urls)
-    print(all_urls)
+        id_urls = [
+                    urljoin(response.url,
+                            parsed_obj.find("a")["href"]) for parsed_obj in parsed_objs
+                  ]
+        all_urls.extend(id_urls)
+    return all_urls
     
-    
+
+def parse_book_page(response):
+    soup = BeautifulSoup(response.text,
+                         "lxml")
+    title = soup.find("h1").text
+    book_title, author = title.split("::")
+    relativ_image_link = soup.find("div",
+                                   class_="bookimage").find("img")["src"]
+    image_link = urljoin(response.url,
+                         relativ_image_link)
+
+    comments_tags = soup.find_all("div", class_="texts")
+    comments = [comments_tag.find("span", class_="black").text
+                for comments_tag in comments_tags]
+
+    genres_tags = soup.find("span", class_="d_book").find_all("a")
+    genres = [tag.text for tag in genres_tags]
+    return {"book_title": sanitize_filename(book_title.strip()),
+            "image_link": image_link,
+            "author": author.strip(),
+            "comments": "\n".join(comments),
+            "genres": "\n".join(genres)}
+            
+
 def main():
     page_numbers = 10
     responses = []
     for page_number in range(page_numbers):  
         try:
-            response = get_response(page_number)
+            page_url = f"https://tululu.org/l55/{str(page_number)}"
+            response = get_response(page_url)
             responses.append(response)
         except requests.exceptions.HTTPError:
             print("Необходимый файл отсутствует")
         except requests.exceptions.ConnectionError:
             time.sleep(1)
         continue    
-    parse_book_page(responses)
+    book_urls = get_book_urls(responses)
+    print(book_urls)
+    for book_url in book_urls:
+        try:
+            resp = get_response(book_url)
+            book_info = [parse_book_page(resp)]
+        except requests.exceptions.HTTPError:
+            print("Необходимый файл отсутствует")
+        except requests.exceptions.ConnectionError:
+            time.sleep(1)
+        continue
+    print (book_info)
+            
+    
 
 if __name__ == "__main__":
     main()
