62269299cb94dbea49918057d04aa6bb16f82168
svgen83 <61458549+svgen83@users.noreply.github.com>
2022-12-10 17:43:44 +0400

    получил ссылки с нескольких страниц

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -13,32 +13,45 @@ def check_for_redirect(response):
     if response.url == "https://tululu.org/":
         raise requests.exceptions.HTTPError
 
-def get_response():
-    url = "https://tululu.org/l55/"
+def get_response(page_number):
+    responses = []
+    url = f"https://tululu.org/l55/{str(page_number)}"
     response = requests.get(url)
     response.raise_for_status()
+    print(response.url)
     check_for_redirect(response)
     return response
 
 
-def parse_book_page(response):
-    url = "https://tululu.org/l55/"
-    soup = BeautifulSoup(response.text,
+def parse_book_page(responses):
+    all_urls = []
+    for response in responses:
+        soup = BeautifulSoup(response.text,
                          "lxml")
-    parsed_objects = soup.find("body").find("div", id = "content").find_all(
+        parsed_objs = soup.find("body").find("div", id = "content").find_all(
     "table")
-    for parsed_object in parsed_objects:
-        id_book = parsed_object.find("a")["href"]
-        id_url = urljoin(url, id_book)
-        print(id_url)
+        id_urls = []
+        for parsed_obj in parsed_objs:
+            id_book = parsed_obj.find("a")["href"]
+            id_url = urljoin(response.url, id_book)
+            id_urls.append(id_url)
+        all_urls.append(id_urls)
+    print(all_urls)
     
     
-
 def main():
-    response = get_response()
-    
-    parse_book_page(response)
-
+    page_numbers = 10
+    responses = []
+    for page_number in range(page_numbers):  
+        try:
+            response = get_response(page_number)
+            responses.append(response)
+        except requests.exceptions.HTTPError:
+            print("Необходимый файл отсутствует")
+        except requests.exceptions.ConnectionError:
+            time.sleep(1)
+        continue    
+    parse_book_page(responses)
 
 if __name__ == "__main__":
     main()
