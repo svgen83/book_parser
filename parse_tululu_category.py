c34fec196dc71bdbbbb468fee0d7f02735eab584
svg83 <svgeneraloff@gmail.com>
2022-12-12 16:52:07 +0400

    добавлено закачивание книг со всех страниц

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -1,12 +1,16 @@
 import argparse
+import json
 import os
 import pathlib
 import requests
+import re
 import time
 
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
-from urllib.parse import urljoin
+from urllib.parse import urljoin, urlparse
+
+from pprint import pprint
 
 
 def check_for_redirect(response):
@@ -42,6 +46,15 @@ def parse_book_page(response):
     book_title, author = title.split("::")
     relativ_image_link = soup.find("div",
                                    class_="bookimage").find("img")["src"]
+    
+    links = soup.find("body").find(class_="d_book").find_all('a')
+    for link in links:
+        if link.text == "скачать txt":
+            relativ_txt_link = link['href']
+
+    txt_link = urljoin(response.url,
+                       relativ_txt_link)
+    
     image_link = urljoin(response.url,
                          relativ_image_link)
 
@@ -51,18 +64,31 @@ def parse_book_page(response):
 
     genres_tags = soup.find("span", class_="d_book").find_all("a")
     genres = [tag.text for tag in genres_tags]
-    return {"book_title": sanitize_filename(book_title.strip()),
+    return {"txt_url": txt_link,
+            "book_title": sanitize_filename(book_title.strip()),
             "image_link": image_link,
             "author": author.strip(),
             "comments": "\n".join(comments),
             "genres": "\n".join(genres)}
+
+
+def download_file(file_url, directory, book_id, book_name, ext):    
+    response = requests.get(file_url)
+    response.raise_for_status()
+    check_for_redirect(response)
+    pathlib.Path(directory).mkdir(parents=True, exist_ok=True)
+    filepath = os.path.join(directory,
+                            f"{book_id}_{book_name}.{ext}")
+    with open(filepath,
+              "wb") as file:
+        file.write(response.content)
             
 
 def main():
     page_numbers = 4
     responses = []
-    books_info = []
-    for page_number in range(page_numbers+1):
+    books_description = []
+    for page_number in range(1, page_numbers+1):
         try:
             page_url = f"https://tululu.org/l55/{str(page_number)}"
             response = get_response(page_url)
@@ -73,20 +99,39 @@ def main():
             time.sleep(1)
         continue    
     book_urls = get_book_urls(responses)
-    print(book_urls)
+
     for book_url in book_urls:
         try:
             resp = get_response(book_url)
-            book_info = parse_book_page(resp)
-            books_info.append(book_info)
+            book_description = parse_book_page(resp)
+            
+            parsed_book_url = urlparse(book_url)
+            book_url_path = parsed_book_url.path
+            book_id = book_url_path.replace('/','').replace('b','')
+
+            download_file(book_description["txt_url"],
+                          "./books",
+                          book_id,
+                          book_description["book_title"],
+                          ext = "txt")
+
+            download_file(book_description["image_link"],
+                          "./Images",
+                          book_id,
+                          book_description["book_title"],
+                          ext = "jpg")
+            
+            books_description.append(book_description)
         except requests.exceptions.HTTPError:
             print("Необходимый файл отсутствует")
         except requests.exceptions.ConnectionError:
             time.sleep(1)
         continue
-    print (books_info)
-            
-    
+##    books_description_json = json.dumps(books_description,
+##                                   ensure_ascii=False).encode('utf8')
+##    with open("./books_description.json", "wb") as file:
+##        file.write(books_description_json)
+
 
 if __name__ == "__main__":
     main()
